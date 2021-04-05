c08c8d875b630fa3c91e9e58bd5553121c40c136
mukesh tiwari <mukeshtiwari.velociters@gmail.com>
2021-04-05 15:06:43 -0700

    added linked list and shape java files of chapter 6 by mukesh

---
+++ testProject/src/com/velociter/training/mukesh/chapter6/shape/Rectangle.java
@@ -0,0 +1,28 @@
+package com.velociter.training.mukesh.chapter6.shape;
+
+public class Rectangle extends Shape {
+// Constructor
+  public Rectangle(Point startDiag, Point endDiag) {
+    // Position of rectangle is top left corner - minimum x and y:
+    position = new Point(Math.min(startDiag.x,endDiag.x),Math.min(startDiag.y,endDiag.y));
+
+    // Bottom right is relative to top left:
+    bottomRight = new Point(Math.max(startDiag.x,endDiag.x) - position.x,
+                            Math.max(startDiag.y,endDiag.y) - position.y);
+  }
+
+  // Overrides the method inherited from Object:
+  public String toString()  { 
+    // Return a string representation of the object:
+    return "Rectangle: Top Left: " + position + " Bottom Right: " + 
+                                                     position.add(bottomRight);
+  }
+
+  // Display the rectangle:  
+  public void show() {				
+    System.out.println("\n" + toString());
+  }
+
+  // Bottom right of rectangle is defined relative to the reference point, position:
+  private Point bottomRight;        // Bottom right of rectangle. 
+}
