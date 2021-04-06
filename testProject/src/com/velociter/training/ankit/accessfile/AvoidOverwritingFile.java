ab516ae5b404dcc4d2ac02054797b1a76f32176d
Ankit <ankit.velociters@gmail.com>
2021-04-06 17:35:05 +0530

    AvoidOverwritingThreeDigit.java file

--- testProject/src/com/velociter/training/ankit/accessfile/AvoidOverwritingFile.java
+++ testProject/src/com/velociter/training/ankit/accessfile/AvoidOverwritingFile.java
@@ -1,41 +1,29 @@
 package com.velociter.training.ankit.accessfile;
-
+//Problem 1
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 
 public class AvoidOverwritingFile {
   public static void main(String[] args) {
-    String filepath = "E:\\New folder\\myFile.txt";
-
-    if(args.length>0) {                                                                 // ***
-      filepath = args[0];                                                               // ***
-    }
-      
+    String filepath = "E:\\New folder\\newCreation.java";
     File aFile = new File(filepath);
-    FileOutputStream outputFile = null;          // Stores the stream reference
+    FileOutputStream outputFile = null;         
     if (aFile.isFile()) {
-      File newFile = aFile;                      // Start with the original file
-
-      // We will append "_old" to the file
-      // name repeatedly until it is unique
+      File newFile = aFile;                     
       do {
-        String name = newFile.getName();         // Get the name of the file
-        int period =  name.indexOf('.');         // Find the separator for the extension
-        if(period == -1) {                                                              // ***
-          newFile = new File(newFile.getParent(), name + "_old");                       // ***
-         }else {                                                                        // ***        
-          newFile = new File(newFile.getParent(), 
-                           name.substring(0, period) + "_old" 
-                           + name.substring(period));
+        String name = newFile.getName();       
+        int period =  name.indexOf('.');       
+        if(period == -1) {                                                             
+          newFile = new File(newFile.getParent(), name + "_old");                      
+         }else {                                                                               
+          newFile = new File(newFile.getParent(),name.substring(0, period) + "_old" + name.substring(period));
          }
-      } while (!aFile.renameTo(newFile));        // Stop when renaming works
+      } while (!aFile.renameTo(newFile));        
     } 
 
-    // Now we can create the new file
     try {
 
-      // Create the stream opened to append data
       outputFile = new FileOutputStream(aFile);
       System.out.println(aFile.getName()+" output stream created");
     } catch (FileNotFoundException e) {
