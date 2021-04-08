2cb78174ab17c47a0785a00e92748e1035748e8f
Ankit <ankit.velociters@gmail.com>
2021-04-08 18:54:33 +0530

    Modification Done into ReadContaintFile

--- testProject/src/com/velociter/training/ankit/readingfiles/ReadContaintFile.java
+++ testProject/src/com/velociter/training/ankit/readingfiles/ReadContaintFile.java
@@ -1,44 +1,84 @@
 package com.velociter.training.ankit.readingfiles;
-//5th
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 public class ReadContaintFile {
 
 	public static void main(String[] ar)
 	{
-		//Path path = Paths.get("E:\\New folder\\WriteDates.txt");
-		Path path= Paths.get("E:\\New folder\\Address.txt");
-		Scanner input= new Scanner(System.in);
-		System.out.println("Enter name and address");
-		String containt[]=new String[2];
-		for(int i=0;i<containt.length;i++)
-		{
-		containt[i] =input.nextLine();
-		}
-		for(int i=0;i<containt.length;i++)
-		try {
-			Files.write(path, containt[i].getBytes(),StandardOpenOption.APPEND);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("data written");
-			try
-			{
-			List<String> data=Files.readAllLines(path);
-			for(String result:data)
-			{
-				System.out.println(" "+result);
-			}
-			
-		} catch (Exception e1) {
+		String filePath = "E:\\New folder\\AddressPeople.txt";
 		
-			e1.printStackTrace();
-		}
-	}
+		File file = new File(filePath);
+		FileWriter fr;
+		Scanner inputData= new Scanner(System.in);
+        
+        System.out.println("How many address name and address");
+        Scanner numberOfdata=new Scanner(System.in);
+        final int person=numberOfdata.nextInt();
+        String containt[]=new String[person];
+         String firstName[]=new  String[person];
+         String surName[]=new  String[person];
+         String address[]=new  String[person];
+         for(int i=0;i<containt.length;i++)
+         {
+        	 System.out.println((i+1)+" Input...........");
+        	 System.out.println("Enter Name");
+             firstName[i] =inputData.nextLine();
+             System.out.println("Enter Surname");
+             surName[i] =inputData.nextLine();
+             System.out.println("Enter Address");
+              address[i] =inputData.nextLine();
+        	 
+         }
+         
+        try {
+			fr = new FileWriter(file, true);
+			
+		 
+		BufferedWriter br = new BufferedWriter(fr);
+		
+        for(int i=0;i<containt.length;i++)
+       {
+        	//surName[count]=surName[i];
+       containt[i] =firstName[i]+"  "+address[i];
+        br.write(containt[i]+String.format("%n"));
+        
+       }		
+        System.out.println("File Created");
+        br.close();
+		fr.close();
+		
+		
+		
+		
+//.......................................................................................
+		//Reading File
+        System.out.println("File Reading Data");
+        FileInputStream fileInputStram=null;
+		 try{    
+			 
+			 fileInputStram=new FileInputStream(filePath);   
+	            int i=0;
+	            while((i=fileInputStram.read())!=-1)   //last character of file
+	            {    
+	                System.out.print((char)i);    
+	            }    
+	            fileInputStram.close();    
+	          }
+		 catch(Exception e)
+		 {
+	          System.out.println(e);
+	          e.printStackTrace();}
+        }
+        catch(Exception e)
+		 {
+	          System.out.println(e);
+	          e.printStackTrace();}
+       }
+
+        
 	}
 
