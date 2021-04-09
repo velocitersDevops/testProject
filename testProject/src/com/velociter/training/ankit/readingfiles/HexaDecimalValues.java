a302ddaab7833fbb12fd7b5224f3e2292ff3e39e
aalam <aalam.velociters@gmail.com>
2021-04-09 12:26:10 +0530

    after complete HexaDecimalValues.java file submit

--- testProject/src/com/velociter/training/ankit/readingfiles/HexaDecimalValues.java
+++ testProject/src/com/velociter/training/ankit/readingfiles/HexaDecimalValues.java
@@ -8,25 +8,28 @@ public class HexaDecimalValues
 {
 	public static void main(String ar[]) throws FileNotFoundException
 	{  
-		String filePath = "E:\\New folder\\Hexadecimal.txt";
+		String filePath = "E:\\hexa\\Hexadecimal.txt";
 	   FileInputStream fileInputStream=new FileInputStream(filePath);
 		int i = 0;
-         int count = 0;
-         int lineChange=0;
+        int hexaValue = 0;
+        int groupValue=0;
           try {
-			while ((i = fileInputStream.read()) != -1) {
-				 System.out.printf("%1X ", i);
-			    count++;
-                if (count ==8) {
+			while ((i = fileInputStream.read()) != -1)
+			{
+				 System.out.printf("%1X ", i);     //print element with 1space 
+				 hexaValue++;
+                if (hexaValue ==8)                 //condition for give extra space when group of 8 hexa value completed
+                {
                 	
 			        System.out.print("    ");
-			        count = 0;
-			       lineChange++;
+			        hexaValue = 0;                //again hexaValue will be 0 to start from 0 to 8
+			        groupValue++;                  //lineChange 
 			    }
                 
-                if (lineChange==5) {
+                if (groupValue==5)                //when group value 5 then jump to next line
+                {
 			        System.out.println();
-			        lineChange = 0;
+			        groupValue = 0;               //again groupValue will be 0 to start from 0 to 5
 			    }
 			}
 		} catch (IOException e) {
