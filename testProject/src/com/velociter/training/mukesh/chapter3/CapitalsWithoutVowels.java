344a0d846cff6f717b2129beb29be0ad2718ce58
mukesh tiwari <mukeshtiwari.velociters@gmail.com>
2021-04-20 20:38:18 -0700

    generate of capital letter ignore vowels of chapter 3

---
+++ testProject/src/com/velociter/training/mukesh/chapter3/CapitalsWithoutVowels.java
@@ -0,0 +1,37 @@
+package com.velociter.training.mukesh.chapter3;
+
+public class CapitalsWithoutVowels 
+{
+		  public static void main(String args[])   
+		  {
+			    int counter = 0;                
+			    int numberToGenerate = 20;     
+			    char alphabateSymbol = 0;               
+			
+			    
+			    while( counter < numberToGenerate )
+			    {
+				      
+				    alphabateSymbol = (char) (26*Math.random() + 'A');               //generate 0-25 randomly number and cast to character
+				   
+				      switch(alphabateSymbol) 
+				      {
+					        //Vowels are ignored if generated 
+					        case 'A':
+					        case 'E':
+					        case 'I':
+					        case 'O':
+					        case 'U':
+					          break;
+					        
+					        default:
+					          
+							        	//Consonant are displayed
+							          System.out.print(alphabateSymbol + " ");
+							          counter++;
+							          break;
+				      }
+			    }
+		  }
+}
+
