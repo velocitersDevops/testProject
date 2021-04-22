07963e62371fff4cfea43e03ee7030b9cede7dd3
Ankit <ankit.velociters@gmail.com>
2021-04-22 19:33:14 +0530

    RandomAccessFile added by ankit

--- testProject/src/com/velociter/training/ankit/writingfiles/PermuteWord.java
+++ testProject/src/com/velociter/training/ankit/writingfiles/PermuteWord.java
@@ -1,100 +1,113 @@
 package com.velociter.training.ankit.writingfiles;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
 public class PermuteWord {
 	
- static void printPermutn(String string)
- {
-	 if(string.length()==0)
-	 {
-		 System.out.println("String is Null");
-		 System.exit(0);
-	 
-	 }
-	 String []wordString=string.split(" ");
-	 int wordLength=wordString.length;
-	 int index=0;
-	 System.out.println(string.length());
-		String[] extract=new String[wordLength];
-		for(String w:wordString)
+	static String inputString = "Hello this is Ankit";
+	  static ByteBuffer byteBufferObject = null;
+	  static FileChannel fileChannelWriteObject = null;
+	  static String[] words = null;                              //to hold actual string what we passed
+	  static String[] permutationWord = null;                    //to hold actual permutation
+	  static int numberOfWords = -1;                             // to represent how many word 
+
+		private static void permute(int index) 
 		{
-			extract[index]=w;
-			index++;
-		}
-		
-		for(String e:extract)
-		{
-			System.out.println(e);
+			 //System.out.println(words[numberOfWords - 1]);
+			if( ++numberOfWords == 0)
+			{
+				permutationWord[index] = null;
+			}else
+			{
+				permutationWord[index] =  words[numberOfWords - 1];	//inserting permutation string in Array
+			}
 			
-		}
-		
-		for (int i = 0; i < wordLength; i++) {
-           String startWord=wordString[i];
-	       int startWordLength=startWord.length();
-           System.out.println(string.substring(i+1, wordLength));
-	    }
- }
-		/* 
-		for (int i=0;i <extract.length-1; i++) 
-	        {
-	            for (int j = 0; j <extract.length-1-i; j++) { 
-	                if (extract[j].compareToIgnoreCase(extract[j+1])>0) 
-	                {
-	             String temprary= extract[j];
-	                	extract[j] = extract[j+1];
-	                	extract[j+1] = temprary;
-	                }
-	            }
-	            for(String e:extract)
+			
+			if (numberOfWords == words.length)    //means 3 == 3
+			{
+				write();
+			} 
+			else   //it execute when no words
+			{
+				for (int i = 0; i < words.length; i++) 
 				{
-					System.out.print(e+" ");
-					
+					if (permutationWord[i] == null)
+					{
+						permute(i);
+					}
 				}
-	            System.out.println();
-	 }
-	 */
-//		 System.out.println("After ");
-//		 for(String e:extract)
-//			{
-//				System.out.print(e+" ");
-//				
-//			}
- //}
-	
-  
-    // Driver code
-    public static void main(String[] args)
-    {
-        String s = "Ankit mat";
-        printPermutn(s);
-       
-    }
+			}
+			numberOfWords--;  //its a temprary var.again initialize
+			permutationWord[index] = null;
+		}
+
+	// Writes a permutation to the file
+		  private static void write() 
+		  {
+				CharBuffer charBuffer = byteBufferObject.asCharBuffer(); // taking character one by one from byteBufferObject												
+				System.out.println(charBuffer);
+				for (int i = 0; i < permutationWord.length; i++)
+				{
+					charBuffer.put(i == 0 ? '\n' : ' ').put(permutationWord[i]);
+				}
+		    try {
+		    	fileChannelWriteObject.write(byteBufferObject);           //here writeng Permutation s String into file
+		    } catch (IOException e) {
+		       System.out.println("Have some issues.please try later");
+		        System.exit(1);
+		    } 
+		    byteBufferObject.clear();
+		  }
+
+
+	  public static void main(String args[]) 
+	  {
+		//get the word from inputString(actual string) into an array
+			words = inputString.split(" ");                 //to get size of array
+			int wordCount = words.length;
+			//System.out.println(wordCount);
+			words =new String[wordCount];
+			words = inputString.split(" ");
+			permutationWord = new String[wordCount];        
+
+			int fromIndex = 0;                               
+			int toIndex = 0;                                 
+			int index = 0;                                   
+	    while((toIndex = inputString.indexOf(' ', fromIndex)) != -1)  
+	    {
+	      words[index++] = inputString.substring(fromIndex,toIndex); 
+	      fromIndex = toIndex;
+	      while(inputString.charAt(++fromIndex) == ' ');        
+	        
+	    }
+
+	    // Now create and write the file
+	    String filePath="E:\\New folder\\PermuteWords.txt";
+      File fileObject = new File(filePath);
+      FileOutputStream fileOutputStreamObject =null;	 
+     
+      try 
+      {
+      	fileOutputStreamObject = new FileOutputStream(fileObject);
+	    } 
+      catch (FileNotFoundException e) 
+      {
+	     System.out.println("File Not Found.Please Check");
+	     System.exit(1);
+	    }
+	    fileChannelWriteObject = fileOutputStreamObject.getChannel();
+	    byteBufferObject = ByteBuffer.allocate(512);         
+	    permute(0);                                          
+
+	    try {
+	    	fileOutputStreamObject.close();
+	    } catch (IOException e) {
+	      e.printStackTrace(System.err);
+	      System.exit(1);
+	    }
+	  }
 }
-
-
-
-
-
-/*
-static void printPermutn(String str, String ans)
-{
-
-    // If string is empty
-    if (str.length() == 0) {
-        System.out.print(ans + " ");
-        return;
-    }
-
-    for (int i = 0; i < str.length(); i++) {
-
-        // ith character of str
-        char ch = str.charAt(i);
-
-        // Rest of the string after excluding 
-        // the ith character
-        String ros = str.substring(0, i) + 
-                     str.substring(i + 1);
-
-        // Recurvise call
-        printPermutn(ros, ans + ch);
-    }
-}*/
